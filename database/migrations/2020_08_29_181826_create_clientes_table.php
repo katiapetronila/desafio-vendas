65747626a15c5b0f8acd4f1a64e57ea2f7ba9893
katiapetronila <katia.petronila1@gmail.com>
2020-08-29 14:11:32 -0500

    add controllers migrates e models

---
+++ database/migrations/2020_08_29_181826_create_clientes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 100);
+            $table->string('telefone', 20);
+            $table->string('email', 100);
+            $table->string('cpf', 100);
+            $table->string('cep', 100);
+            $table->string('logadouro', 100)->nullable();
+            $table->string('bairro', 100)->nullable();
+            $table->string('localidade', 100)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
---
+++ database/migrations/2020_08_29_182953_create_item_vendas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemVendasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_vendas', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('valor_unitario', 10, 2);
+            $table->decimal('valor_total',10, 2);
+            $table->unsignedBigInteger('venda_id');
+            $table->foreign('venda_id')->references('id')->on('vendas');
+            $table->foreign('produto_id')->references('id')->on("produtos");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_vendas');
+    }
+}
